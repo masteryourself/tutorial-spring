ad7c02a615a2615fddc0747bc867f636f39640ed
masteryourself <renzhao.ruan@foxmail.com>
2020-03-13 21:19:44 +0800

    tutorial-spring-boot-helloworld 工程提交

---
+++ tutorial-spring-boot/tutorial-spring-boot-helloworld/src/main/java/pers/masteryourself/tutorial/spring/boot/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package pers.masteryourself.tutorial.spring.boot.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>description : HelloWorldController
+ *
+ * <p>blog : https://Blog.csdn.net/masteryourself
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2020/3/13 21:18
+ */
+@RestController
+public class HelloWorldController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
