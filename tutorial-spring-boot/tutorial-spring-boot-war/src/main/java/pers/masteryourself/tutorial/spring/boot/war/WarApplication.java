109c3867f36270d25610008aa2fcff612bb85903
masteryourself <renzhao.ruan@foxmail.com>
2020-03-15 21:03:54 +0800

    tutorial-spring-boot-war 工程提交

---
+++ tutorial-spring-boot/tutorial-spring-boot-war/src/main/java/pers/masteryourself/tutorial/spring/boot/war/WarApplication.java
@@ -0,0 +1,22 @@
+package pers.masteryourself.tutorial.spring.boot.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>description : WarApplication
+ *
+ * <p>blog : https://Blog.csdn.net/masteryourself
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2020/3/15 20:53
+ */
+@SpringBootApplication
+public class WarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarApplication.class, args);
+    }
+
+}
