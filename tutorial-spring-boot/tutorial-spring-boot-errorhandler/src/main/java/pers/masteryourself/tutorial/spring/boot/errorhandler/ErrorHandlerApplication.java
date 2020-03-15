8bf203e81ce78de5f14127ffafe5866d6da1d9cf
masteryourself <renzhao.ruan@foxmail.com>
2020-03-15 19:27:43 +0800

    tutorial-spring-boot-errorhandler 工程提交

---
+++ tutorial-spring-boot/tutorial-spring-boot-errorhandler/src/main/java/pers/masteryourself/tutorial/spring/boot/errorhandler/ErrorHandlerApplication.java
@@ -0,0 +1,22 @@
+package pers.masteryourself.tutorial.spring.boot.errorhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>description : ErrorHandlerApplication
+ *
+ * <p>blog : https://Blog.csdn.net/masteryourself
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2020/3/15 16:28
+ */
+@SpringBootApplication
+public class ErrorHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorHandlerApplication.class, args);
+    }
+
+}
