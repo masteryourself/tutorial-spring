b949222cd93c18f66cc92f44ed57f1e67db7abcd
masteryourself <renzhao.ruan@foxmail.com>
2020-03-16 22:53:24 +0800

    tutorial-spring-boot-mybatis 拆分成 annotation 和 xml 版本

---
+++ tutorial-spring-boot-integration/tutorial-spring-boot-mybatis-annotation/src/main/java/pers/masteryourself/tutorial/spring/boot/mybatis/MyBatisAnnotationApplication.java
@@ -0,0 +1,24 @@
+package pers.masteryourself.tutorial.spring.boot.mybatis;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>description : MyBatisAnnotationApplication
+ *
+ * <p>blog : https://Blog.csdn.net/masteryourself
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2020/3/16 22:41
+ */
+@SpringBootApplication
+@MapperScan(value = "pers.masteryourself.tutorial.spring.boot.mybatis.mapper")
+public class MyBatisAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisAnnotationApplication.class, args);
+    }
+
+}
---
+++ tutorial-spring-boot-integration/tutorial-spring-boot-mybatis-annotation/src/main/resources/sql/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user`(user_name) VALUES ('张三');
+INSERT INTO `user`(user_name) VALUES ('李四');
+INSERT INTO `user`(user_name) VALUES ('王五');
