d7e8f24d531419322c9e3ed6feed3d129007befb
masteryourself <renzhao.ruan@foxmail.com>
2020-03-29 23:33:18 +0800

    tutorial-spring-framework-listener 工程提交

---
+++ tutorial-spring-framework/tutorial-spring-framework-listener/src/main/java/pers/masteryourself/tutorial/spring/framework/listener/config/MyApplicationListener.java
@@ -0,0 +1,24 @@
+package pers.masteryourself.tutorial.spring.framework.listener.config;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>description : MyApplicationListener
+ *
+ * <p>blog : https://Blog.csdn.net/masteryourself
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2020/3/29 22:56
+ */
+@Component
+public class MyApplicationListener implements ApplicationListener<ApplicationEvent> {
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        System.out.println("=============== MyApplicationListener 接收到事件：" + event.getClass());
+    }
+
+}
---
+++ tutorial-spring-framework/tutorial-spring-framework-listener/src/main/java/pers/masteryourself/tutorial/spring/framework/listener/config/SpringConfig.java
@@ -0,0 +1,18 @@
+package pers.masteryourself.tutorial.spring.framework.listener.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>description : SpringConfig
+ *
+ * <p>blog : https://Blog.csdn.net/masteryourself
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2020/3/29 22:57
+ */
+@Configuration
+@ComponentScan
+public class SpringConfig {
+}
