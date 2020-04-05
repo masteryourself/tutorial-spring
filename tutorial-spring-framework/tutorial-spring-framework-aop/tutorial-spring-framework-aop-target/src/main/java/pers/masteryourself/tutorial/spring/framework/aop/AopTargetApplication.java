98d26170ea518e0d4fa28cebf969cd28676ccb80
masteryourself <renzhao.ruan@foxmail.com>
2020-04-06 02:02:13 +0800

    tutorial-spring-framework-aop-target 工程提交

---
+++ tutorial-spring-framework/tutorial-spring-framework-aop/tutorial-spring-framework-aop-target/src/main/java/pers/masteryourself/tutorial/spring/framework/aop/AopTargetApplication.java
@@ -0,0 +1,32 @@
+package pers.masteryourself.tutorial.spring.framework.aop;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import pers.masteryourself.tutorial.spring.framework.aop.config.SpringConfig;
+import pers.masteryourself.tutorial.spring.framework.aop.service.PersonService;
+import pers.masteryourself.tutorial.spring.framework.aop.service.impl.PersonServiceImpl;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * <p>description : AopTargetApplication
+ *
+ * <p>blog : https://Blog.csdn.net/masteryourself
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2020/4/6 1:57
+ */
+public class AopTargetApplication {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        PersonService personService = context.getBean(PersonService.class);
+        personService.query("张三");
+        System.out.println("*****************************分割线*****************************");
+        personService.save("张三");
+        // 使用 jdk 动态代理：true, false
+        System.out.println(personService instanceof Proxy);
+        System.out.println(personService instanceof PersonServiceImpl);
+    }
+
+}
