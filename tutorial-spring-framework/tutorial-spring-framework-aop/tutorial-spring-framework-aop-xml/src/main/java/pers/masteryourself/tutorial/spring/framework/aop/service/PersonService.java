6e7d4af8f675bbc1fb69f2bb512653ced109f63f
masteryourself <renzhao.ruan@foxmail.com>
2020-04-06 13:50:34 +0800

    tutorial-spring-framework-aop-xml 工程提交

---
+++ tutorial-spring-framework/tutorial-spring-framework-aop/tutorial-spring-framework-aop-xml/src/main/java/pers/masteryourself/tutorial/spring/framework/aop/service/PersonService.java
@@ -0,0 +1,23 @@
+package pers.masteryourself.tutorial.spring.framework.aop.service;
+
+/**
+ * <p>description : PersonServiceImpl
+ *
+ * <p>blog : https://Blog.csdn.net/masteryourself
+ *
+ * @author : masteryourself
+ * @version : 1.0.0
+ * @date : 2020/4/6 0:30
+ */
+public class PersonService {
+
+    public void query(String name) {
+        System.out.println("执行 query()");
+    }
+
+    public String save(String name) {
+        System.out.println("执行 save()");
+        return "save() 成功";
+    }
+
+}
